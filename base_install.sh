794bee8ad524a1700b4b39a417760a1a952086cc
Roan Song <roansong@gmail.com>
2018-05-12 16:06:35 +0200

    Initial commit, with just zsh, git, and an xrandr command to set refresh rate

---
+++ base_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# List of commands to execute in a new Linux environment
+
+# Things to install: zsh, git
+
+sudo apt-get update
+sudo apt-get updgrade
+sudo apt-get -y install zsh git
+
+mkdir ~/development
---
+++ refresh_rate.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Set display refresh rate to 144hz
+xrandr --rate 144.00
